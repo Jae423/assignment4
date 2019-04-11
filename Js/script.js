efe7bd572f8db25c5310e671d0897682aa8922d0
Jae423 <45524635+Jae423@users.noreply.github.com>
2019-04-11 19:26:30 -0400

    3 update

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVsbGkxMCIsImEiOiJjanVkOXE0b3IwdDM4NDRxcmw3ZzJuc201In0.YpaZ6sRzl9kVVJwJpcK9lA';
+
+
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v9'
+});
+</script>
