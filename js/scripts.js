1ca30d5744859acc259b2e5e00ebe8dd52391522
Jae423 <45524635+Jae423@users.noreply.github.com>
2019-04-25 18:59:12 -0400

    tet

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,6 @@
 // sets up my mapbox access token so they can track my usage of their basemap services
 mapboxgl.accessToken = 'pk.eyJ1IjoiamVsbGkxMCIsImEiOiJjanVkOXE0b3IwdDM4NDRxcmw3ZzJuc201In0.YpaZ6sRzl9kVVJwJpcK9lA';
 
-var map = new mapboxgl.Map({
-  container: 'mapContainer',
-  style: 'mapbox://styles/mapbox/light-v9',
-  center: [-73.98, 40.70],
-  zoom: 10,
-});
-
 // instantiate the map
 var map = new mapboxgl.Map({
   container: 'mapContainer',
