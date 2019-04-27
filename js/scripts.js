66c5f81eaf0488a71e14fe7ca008f8bfca63e6a5
Jae423 <45524635+Jae423@users.noreply.github.com>
2019-04-26 21:27:04 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 // sets up my mapbox access token so they can track my usage of their basemap services
-mapboxgl.accessToken = 'pk.eyJ1IjoiamVsbGkxMCIsImEiOiJjanVkOXE0b3IwdDM4NDRxcmw3ZzJuc201In0.YpaZ6sRzl9kVVJwJpcK9lA';
-
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVsbGkxMCIsImEiOiJjanVkOXE0b3IwdDM4NDRxcmw3ZzJuc201In0.YpaZ6sRzl9kVVJwJpcK9lA'
 // instantiate the map
 var map = new mapboxgl.Map({
   container: 'mapContainer',
